f692d197fd7ae4861a7381a624cab84750cd8d74
Yuri <yuri.tayama@linksports.co.jp>
2018-08-08 23:19:16 +0900

    aタグのノードだけを抽出

--- parse.go
+++ parse.go
@@ -21,17 +21,21 @@ func Parse(r io.Reader) ([]Link, error) {
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
+	nodes := linkNodes(doc)
+	// _にはindexが入ってくる
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
 	return nil, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	msg := n.Data
-	if n.Type == html.ElementNode {
-		msg = "<" + msg + ">"
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
 	}
-	fmt.Println(padding, n.Data)
+	var ret []*html.Node
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+"  ")
+		ret = append(ret, linkNodes(c)...)
 	}
+	return ret
 }
